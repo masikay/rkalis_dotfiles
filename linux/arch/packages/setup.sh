d0b2bf7e21db5443e21d46a87fb5e1516e331dc2
Paolo Astolfi <paolo.astolfi@gmail.com>
2020-04-04 17:32:29 +0200

    Fixes for Arch/Manjaro

--- linux/arch/packages/setup.sh
+++ linux/arch/packages/setup.sh
@@ -13,4 +13,6 @@ info "Installing Arch/Manjaro packages..."
 sudo pacman -Syu --noconfirm
 sudo pacman -Sy --noconfirm yay
 yay -S --needed --noconfirm - < pkglist
+sudo pacman -R vi --noconfirm
+sudo ln -sf  /usr/bin/vim /usr/bin/vi
 success "Finished installing Arch/Manjaro packages."
